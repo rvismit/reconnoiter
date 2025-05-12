818fdc4e1941dbd43cd92496c9ee5f2dd1f7cd67
Druk <26144028+rvismit@users.noreply.github.com>
2025-05-13 01:39:52 +0530

    Update reconnoiter.sh

--- reconnoiter.sh
+++ reconnoiter.sh
@@ -4,7 +4,7 @@
 # Reconnoiter: https://github.com/rvismit/reconnoiter
 # twitter: https://www.twitter.com/th3_druk
 # Created Dec 2019 | Monastery
-# Updated on Oct 2022
+# Updated on May 2025
 # New updates coming soon.
 
 echo            "  ____                                  _ _"
@@ -21,14 +21,13 @@ read vardomain
                 host $vardomain
                 then
                 echo "Resolved"
-        curl  https://api.hackertarget.com/hostsearch/?q=$vardomain | cut -d',' -f1 | sort -u | grep $vardomain >> subdomains.txt
-        curl  http://web.archive.org/cdx/search/cdx?url=*.$vardomain/\&output=text\&fl=original\&collapse=urlkey | grep $vardomain | sed -e 's_https*://__' -e "s/\/.*//" >> subdomains.txt        
-	curl  http://index.commoncrawl.org/CC-MAIN-2018-22-index?url=*.$vardomain\&output=json | jq -r .url | sort -u >>  subdomains.txt
-        curl  https://api.threatminer.org/v2/domain.php?q=$vardomain\&rt=5 | jq '.' | sort -u | grep $vardomain >>  subdomains.txt
-        curl  https://riddler.io/search?q=pld:$vardomain | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u >>  subdomains.txt
+        curl -s https://api.hackertarget.com/hostsearch/?q=$vardomain | cut -d',' -f1 | sort -u | grep $vardomain >> subdomains.txt
+        curl -s http://web.archive.org/cdx/search/cdx?url=*.$vardomain/\&output=text\&fl=original\&collapse=urlkey | grep $vardomain | sed -e 's_https*://__' -e "s/\/.*//" >> subdomains.txt        
+	curl -s http://index.commoncrawl.org/CC-MAIN-2018-22-index?url=*.$vardomain\&output=json | jq -r .url | sort -u >>  subdomains.txt
+        curl -s https://otx.alienvault.com/api/v1/indicators/domain/$vardomain/passive_dns | jq -r '.passive_dns[].hostname' | grep -w "$vardomain$" | sort -u >> subdomains.txt
+        curl -s https://riddler.io/search?q=pld:$vardomain | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u >>  subdomains.txt
         curl -s https://crt.sh/?q=%.$vardomain | grep -oE "[\.a-zA-Z0-9-]+\.$vardomain" | sort -u >>  subdomains.txt
-        curl  https://sonar.omnisint.io/subdomains/$vardomain | jq -r ".[]" | sort -u >>  subdomains.txt
-        curl https://jldc.me/anubis/subdomains/$vardomain | jq -r ".[]" | sort -u >>  subdomains.txt
+        curl -s https://rapiddns.io/subdomain/$vardomain?full=1 | grep -oP "([a-zA-Z0-9_-]+\.)+$vardomain" | sort -u >> subdomains.txt
         webchk -i ./subdomains.txt | grep '200' >> httpstatus.txt #Check website status
         else
                 echo -e "Invalid Host"
